4b65d20ab772971277e0e933f1a0b15bf41f4fdd
GabrielCFranco <gabrielcf06@gmail.com>
2024-08-22 17:10:42 -0300

    Created Status Window, menu changed grid to list, menu navegation now working

---
+++ statuswindow.cpp
@@ -0,0 +1,8 @@
+#include "statuswindow.h"
+StatusWindow::StatusWindow(int height, int width, int row, int col)
+{
+    __height = height; __width = width;
+    __row = row; __col = col;
+    __sWin = newwin(height, width, row, col);
+    box(__sWin, 0, 0);
+}
